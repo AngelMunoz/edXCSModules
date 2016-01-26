fba0a018bb97c55061b82b82ae15fa7c6ae85cb3
Angel Daniel Munoz Gonzalez <scythes.lol@gmail.com>
2016-01-26 01:03:41 -0700

    Initial commit modules 1-2

---
+++ Module0/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string fName;
+            string lName;
+            DateTime birthDate;
+            string addressLine1;
+            string addressLine2;
+            string city;
+            string state;
+            long psCode;
+            string country;
+
+        }
+    }
+}
---
+++ Module2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 1; j < 9; j++)
+                {
+                    switch (i % 2)
+                    {
+                        case 0:
+                            if (j % 2 != 0)
+                            {
+                                Console.Write("X");
+                            }
+                            else
+                            {
+                                Console.Write("O");
+                            }
+                            break;
+                        default:
+                            if (j % 2 == 0)
+                            {
+                                Console.Write("X");
+                            }
+                            else
+                            {
+                                Console.Write("O");
+                            }
+                            break;
+                    }
+                }
+                Console.Write("\n");
+            }
+            Console.ReadKey();
+        }
+    }
+}
