8854f4d2770c6450f14bc400d2859bf594e8b286
Angel Daniel Munoz Gonzalez <scythes.lol@gmail.com>
2016-01-29 02:59:28 -0700

    Module 9

---
+++ Module9/MainWindow.xaml.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Module9
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private List<Student> _students;
+        private int _currentIndex = 0;
+        public MainWindow()
+        {
+            InitializeComponent();
+            Students = new List<Student>();
+            Students.Add(new Student("Angel", "Munoz", new DateTime(1992, 05, 16), "Dir1", "Juarez", "Chihuahua", 12345, "Mexico"));
+            Students.Add(new Student("Perla", "Lopez", new DateTime(1992, 12, 10), "Dir1", "Juarez", "Chihuahua", 12345, "Mexico", "Dir2"));
+            Students.Add(new Student("Victor", "Munoz", new DateTime(1992, 07, 10), "Dir1", "Juarez", "Chihuahua", 12345, "Mexico"));
+            txtCity.Text = Students[CurrentIndex].City;
+            txtFirstName.Text = Students[CurrentIndex].Fname;
+            txtLastName.Text = Students[CurrentIndex].Lname;
+            btnNext.IsEnabled = true;
+            btnPrevious.IsEnabled = false;
+
+        }
+
+        public List<Student> Students
+        {
+            get
+            {
+                return _students;
+            }
+
+            set
+            {
+                _students = value;
+            }
+        }
+
+        public int CurrentIndex
+        {
+            get
+            {
+                return _currentIndex;
+            }
+
+            set
+            {
+                _currentIndex = value;
+            }
+        }
+
+        private void btnCreateStudent_Click(object sender, RoutedEventArgs e)
+        {
+           
+            Students.Add(new Student(txtFirstName.Text, txtLastName.Text, new DateTime(1992, 05, 20), "", txtCity.Text, "", 12345, ""));
+            txtCity.Clear();
+            txtFirstName.Clear();
+            txtLastName.Clear();
+        }
+
+        private void btnPrevious_Click(object sender, RoutedEventArgs e)
+        {
+            CurrentIndex--;
+            if (CurrentIndex < 0)
+            {
+                CurrentIndex = 0;
+                btnPrevious.IsEnabled = false;
+            }
+            txtCity.Text = Students[CurrentIndex].City;
+            txtFirstName.Text = Students[CurrentIndex].Fname;
+            txtLastName.Text = Students[CurrentIndex].Lname;
+            btnNext.IsEnabled = true;
+           
+        }
+
+        private void btnNext_Click(object sender, RoutedEventArgs e)
+        {
+            CurrentIndex++;
+            if (CurrentIndex >= Students.Count)
+            {
+                CurrentIndex = Students.Count - 1;
+                btnNext.IsEnabled = false;
+            }
+            txtCity.Text = Students[CurrentIndex].City;
+            txtFirstName.Text = Students[CurrentIndex].Fname;
+            txtLastName.Text = Students[CurrentIndex].Lname;
+            btnPrevious.IsEnabled = true;
+           
+        }
+    }
+}
---
+++ Module9/Student.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module9
+{
+    public class Student:Person
+    {
+        private static long _studentAmt;
+        private Stack<int> _grades;
+
+        public Student(string fname, string lname, DateTime birthdate, string addr1, string city, string state, long zip, string country, string addr2 = "", Stack<int> grades = null)
+            :base(fname, lname, birthdate, addr1, city, state, zip, country, addr2)
+        {
+            Fname = fname;
+            Lname = lname;
+            Birthdate = birthdate;
+            Addr1 = addr1;
+            Addr2 = addr2;
+            City = city;
+            State = state;
+            Zip = zip;
+            Country = country;
+            StudentAmt += 1;
+            Grades = grades;
+        }
+        public void TakeTest()
+        {
+            Console.WriteLine("Student Took Test");
+        }
+        public static long StudentAmt
+        {
+            get
+            {
+                return _studentAmt;
+            }
+
+            set
+            {
+                _studentAmt = value;
+            }
+        }
+
+        public Stack<int> Grades
+        {
+            get
+            {
+                return _grades;
+            }
+
+            set
+            {
+                _grades = value;
+            }
+        }
+    }
+}
